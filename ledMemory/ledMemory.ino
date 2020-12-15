24530ead32d9020e1c1b727269c7c9f07ef425fa
Bernhard Häussner <bxt@die-optimisten.net>
2020-12-15 17:03:05 +0100

    refactor(ledMemory): move some things into functions and add comments

--- ledMemory/ledMemory.ino
+++ ledMemory/ledMemory.ino
@@ -25,12 +25,16 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 // Connect the corresponding LEDs to those pins:
 #define PIN_LED_LEFT 2
 #define PIN_LED_DOWN 3
+// This pin is out of order because otherwise the OLED reset makes it light up:
 #define PIN_LED_UP 6
 #define PIN_LED_RIGHT 5
 
 // Connect the buzzer
 #define BUZZER_PIN A0
 #define GAME_OVER_FREQUENCY 220
+
+// How fast the game runs
+#define DELAY_TIME_MS 500
 
 class DirectionButton {
 private:
@@ -111,93 +115,26 @@ void waitForAnyButtonPressed() {
   }
 }
 
-void loop() {
+void displaySplashScreen() {
   display.clearDisplay();
   display.drawBitmap(16, 8, splash_bmp, SPLASH_BMP_WIDTH, SPLASH_BMP_HEIGHT, 1);
   display.setCursor(13, 55);
   display.println(F("- press any key -"));
   display.display();
+}
 
-  waitForAnyButtonPressed();
+void displayGameInProgressScreen(int score) {
+  display.clearDisplay();
+  display.setCursor(19, 18);
+  display.println(F("listen & repeat"));
+  display.setCursor(48, 32);
+  display.println(F("Score: "));
+  display.setCursor(59, 41);
+  display.println(score);
+  display.display();
+}
 
-  int score = 0;
-  uint8_t lastNewSequenceEntry = 5;
-  for (; score < 100; score++) {
-    display.clearDisplay();
-    display.setCursor(19, 18);
-    display.println(F("listen & repeat"));
-    display.setCursor(48, 32);
-    display.println(F("Score: "));
-    display.setCursor(59, 41);
-    display.println(score);
-    display.display();
-
-    uint8_t newSequenceEntry = random(3);
-    if (newSequenceEntry >= lastNewSequenceEntry) {
-      newSequenceEntry++;
-    }
-    lastNewSequenceEntry = newSequenceEntry;
-    sequence[score / 4] &= ~(3 << ((score % 4) * 2));
-    sequence[score / 4] |= newSequenceEntry << ((score % 4) * 2);
-
-    for (int i = 0; i < score + 1; i++) {
-      uint8_t sequenceEntry = (sequence[i / 4] >> ((i % 4) * 2)) & 3;
-
-      digitalWrite(leds[sequenceEntry], HIGH);
-      tone(BUZZER_PIN, frequencies[sequenceEntry], 500);
-      delay(500);
-      digitalWrite(leds[sequenceEntry], LOW);
-      delay(100);
-    }
-
-    bool everythingCorrect = true;
-    unsigned long lastButtonTime = 0;
-    uint8_t lastButtonLed = leds[0];
-
-    for (int i = 0; i < score + 1; i++) {
-      uint8_t sequenceEntry = (sequence[i / 4] >> ((i % 4) * 2)) & 3;
-
-      bool anyButtonPressed = false;
-      while (!anyButtonPressed) {
-        if (millis() > lastButtonTime + 500) {
-          digitalWrite(lastButtonLed, LOW);
-        }
-        for (int k = 0; k < 4; k++) {
-          if (buttons[k].loopAndIsJustPressed()) {
-            digitalWrite(lastButtonLed, LOW);
-            lastButtonLed = leds[k];
-            anyButtonPressed = true;
-            if (k != sequenceEntry) {
-              everythingCorrect = false;
-              break;
-            }
-            digitalWrite(leds[k], HIGH);
-            tone(BUZZER_PIN, frequencies[k], 500);
-            lastButtonTime = millis();
-          }
-        }
-      }
-
-      if (!everythingCorrect) {
-        break;
-      }
-    }
-
-    if (everythingCorrect) {
-      delay(500);
-      for (int k = 0; k < 4; k++) {
-        digitalWrite(leds[k], LOW);
-      }
-      delay(500);
-    } else {
-      break;
-    }
-  }
-
-  for (int k = 0; k < 4; k++) {
-    digitalWrite(leds[k], HIGH);
-  }
-
+void displayGameOverScreen(int score) {
   display.clearDisplay();
   display.setCursor(42, 18);
   display.println(F("GAME OVER"));
@@ -210,13 +147,105 @@ void loop() {
   }
   display.println(score);
   display.display();
+}
 
-  tone(BUZZER_PIN, GAME_OVER_FREQUENCY, 500);
-  delay(500);
+void turnAllLeds(uint8_t toLevel) {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(leds[i], toLevel);
+  }
+}
+
+void setSequence(uint8_t index, uint8_t value) {
+  uint8_t sequenceIndex = index / 4;
+  uint8_t bitIndex = (index % 4) * 2;
+  sequence[sequenceIndex] &= ~(3 << bitIndex);
+  sequence[sequenceIndex] |= value << bitIndex;
+}
+
+uint8_t getSequence(uint8_t index) {
+  uint8_t sequenceIndex = index / 4;
+  uint8_t bitIndex = (index % 4) * 2;
+  return (sequence[sequenceIndex] >> bitIndex) & 3;
+}
+
+void loop() {
+  displaySplashScreen();
 
   waitForAnyButtonPressed();
 
-  for (int k = 0; k < 4; k++) {
-    digitalWrite(leds[k], LOW);
+  int score = 0;
+  uint8_t lastNewSequenceEntry = 5; // sentinel, but means we never start with last one...
+  for (; score < 100; score++) {
+    displayGameInProgressScreen(score);
+
+    // Pick, and make sure we don't pick the same entry twice in a row:
+    uint8_t newSequenceEntry = random(3);
+    if (newSequenceEntry >= lastNewSequenceEntry) {
+      newSequenceEntry++;
+    }
+    lastNewSequenceEntry = newSequenceEntry;
+
+    setSequence(score, newSequenceEntry);
+
+    // Play back the sequence including the new value:
+    for (int i = 0; i < score + 1; i++) {
+      uint8_t sequenceEntry = getSequence(i);
+      digitalWrite(leds[sequenceEntry], HIGH);
+      tone(BUZZER_PIN, frequencies[sequenceEntry], DELAY_TIME_MS);
+      delay(DELAY_TIME_MS);
+
+      digitalWrite(leds[sequenceEntry], LOW);
+      delay(100);
+    }
+
+    // Read back the sequence from the user:
+    bool everythingCorrect = true;
+    unsigned long lastButtonTime = 0;
+    uint8_t lastButtonLed = leds[0];
+    for (int i = 0; i < score + 1; i++) {
+      uint8_t sequenceEntry = getSequence(i);
+
+      bool anyButtonPressed = false;
+      while (!anyButtonPressed) { // loop until any button was pressed
+        if (millis() > lastButtonTime + DELAY_TIME_MS) {
+          digitalWrite(lastButtonLed, LOW);
+        }
+        for (int k = 0; k < 4; k++) {
+          if (buttons[k].loopAndIsJustPressed()) {
+            digitalWrite(lastButtonLed, LOW);
+            lastButtonLed = leds[k];
+            anyButtonPressed = true;
+            if (k != sequenceEntry) { // wrong button!
+              everythingCorrect = false;
+              break;
+            }
+            digitalWrite(leds[k], HIGH);
+            tone(BUZZER_PIN, frequencies[k], DELAY_TIME_MS);
+            lastButtonTime = millis();
+          }
+        }
+      }
+
+      if (!everythingCorrect) {
+        break;
+      }
+    }
+
+    if (everythingCorrect) {
+      delay(DELAY_TIME_MS);
+      turnAllLeds(LOW);
+      delay(DELAY_TIME_MS);
+    } else {
+      break;
+    }
   }
+
+  turnAllLeds(HIGH);
+  tone(BUZZER_PIN, GAME_OVER_FREQUENCY, DELAY_TIME_MS);
+  displayGameOverScreen(score);
+  delay(DELAY_TIME_MS);
+
+  waitForAnyButtonPressed();
+
+  turnAllLeds(LOW);
 }
